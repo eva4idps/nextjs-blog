46f2058ec0ce963eae3a92b00c6ee13ecb43baba
eva4idps <eva@4idps.com>
2020-10-08 19:44:25 +0800

    Dynamic routes (error)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
 import Head from 'next/head'
-import Layout, {name, siteTitle} from '../components/layout'
+import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-
+import { getSortedPostsData } from '../lib/externalAPIPosts'
+// import defaultAvatar from '../public/images/profile.jpg' 
+//有await就要try catch
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData() //取得已排序的資料
+  let posts = {}
+  let isSuccess = false
+  try {
+    posts = await getSortedPostsData()
+  } catch(e) {
+    console.log('getStaticProps', e.message)
+    isSuccess = false
+    return isSuccess
+  }
+  //api回傳錯誤訊息
+  if (posts.error != 0) {
+    isSuccess = false
+  } else {
+    isSuccess = true
+  }
+  
   return { //當作props回傳
     props: {
-      allPostsData
+      isSuccess,
+      posts
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ isSuccess, posts }) {
+  console.log("posts", posts)
+  console.log("isSuccess", isSuccess)
   return (
-    <Layout home>
+    <Layout avatar={!isSuccess ? `/images/profile.jpg` : posts.user.avatar} name={!isSuccess ? `Alex` : posts.user.display_name}>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello. My name is {name}. Nice to meet you!</p>
+        <p>Hello. My name is {!isSuccess ? `Alex` : posts.user.display_name}. Nice to meet you!</p>
         <p>
         (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -28,17 +47,10 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
+          {Array.isArray(posts)?"做map":posts?.rate}
         </ul>
       </section>
     </Layout>
+    
   )
 }
